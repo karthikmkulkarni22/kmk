4d24ad76d7740434aef0f3e5f8850ae9cb4044ec
Your Name <you@example.com>
2018-07-03 12:18:15 +0530

    commit by kmk

---
+++ selenium/src/test/java/com/tests/test2.java
@@ -0,0 +1,26 @@
+package com.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class test2 {
+	@Test
+	public void testGitHubLogin()
+	{
+		//for downloading exe file of a browser, since browsers are not developed by seleniumhq//
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\training_b7c.06.13\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();//create new object and open browser//
+		driver.manage().window().maximize();// to maximize the browser
+		driver.get("https://github.com/login");
+		driver.findElement(By.id("login_field")).sendKeys("karthikmkulkarni22");
+		driver.findElement(By.name("commit")).click();
+		
+		String title = driver.getTitle();//to check the the title of the page for validations
+		Assert.assertEquals(title, "GitHub");//for validations, if it is false or expected !=actual, testing shouldnt continue or should throw an error
+		System.out.println("continue with testing....");
+	}
+
+}
---
+++ selenium/src/test/java/selenium/NewTest.java
@@ -0,0 +1,16 @@
+package selenium;
+import org.testng.annotations.Test;
+
+public class NewTest {
+		@Test
+		public void testDemoWebShop()
+		{
+			System.out.println("in web shop");
+		}
+		@Test
+		public void testpayment()
+		{
+			System.out.println("in test payment");
+		}
+}
+
